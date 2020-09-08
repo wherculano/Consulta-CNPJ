ddbd3188513d16686c57e207c801cc1c933ae9b4
Wagner Herculano <wagherculano@hotmail.com>
2020-09-08 18:11:42 -0300

    Corrigido CNPJ de exemplo

--- ConsultaCNPJ/tests/test_consulta_cnpj_prompt.py
+++ ConsultaCNPJ/tests/test_consulta_cnpj_prompt.py
@@ -2,13 +2,13 @@ from ConsultaCNPJ.consulta_cnpj_prompt import CnpjPrompt
 
 
 def test_cnpj():
-    numero = '00 000.000-0000/00'
+    numero = '00360305000104'
     cnpj = CnpjPrompt(numero)
     assert cnpj is not None
 
 
 def test_consulta():
-    numero = '00 000.000-0000/00'
+    numero = '00360305000104'
     cnpj = CnpjPrompt(numero)
     result = cnpj.consulta()
     assert result is None
