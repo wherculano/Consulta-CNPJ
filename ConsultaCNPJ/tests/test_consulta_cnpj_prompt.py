63b0cd3a188e08eec18d2b59870d9f6c4ff32c3f
Wagner Herculano <wagherculano@hotmail.com>
2020-09-08 17:58:36 -0300

    Alterando CNPJ de exemplo

--- ConsultaCNPJ/tests/test_consulta_cnpj_prompt.py
+++ ConsultaCNPJ/tests/test_consulta_cnpj_prompt.py
@@ -2,13 +2,13 @@ from ConsultaCNPJ.consulta_cnpj_prompt import CnpjPrompt
 
 
 def test_cnpj():
-    numero = '05 861.888-0001/70'
+    numero = '00 000.000-0000/00'
     cnpj = CnpjPrompt(numero)
     assert cnpj is not None
 
 
 def test_consulta():
-    numero = '05 861.888-0001/70'
+    numero = '00 000.000-0000/00'
     cnpj = CnpjPrompt(numero)
     result = cnpj.consulta()
     assert result is None
